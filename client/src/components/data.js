d82f77c4220e89aaed41bb304a1ed36f974fb642
unknown <sanjin00@gmail.com>
2023-12-23 12:05:39 +0100

    updated

---
+++ client/src/components/data.js
@@ -0,0 +1,41 @@
+import Thumbnail1 from '../images/blog1.jpg'
+import Thumbnail2 from '../images/blog2.jpg'
+import Thumbnail3 from '../images/blog3.jpg'
+import Thumbnail4 from '../images/blog4.jpg'
+
+export const DUMMY_POSTS = [
+    {
+      id: '1',
+      thumbnail: Thumbnail1,
+      category: 'education',
+      title: 'This is the title of the very first post on this blog',
+      desc: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+      authorID: 3
+    },
+    {
+      id: '2',
+      thumbnail: Thumbnail2,
+      category: 'science',
+      title: 'This is the title of the very first post on this blog',
+      desc: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+      authorID: 1
+    },
+    {
+      id: '3',
+      thumbnail: Thumbnail3,
+      category: 'weather',
+      title: 'This is the title of the very first post on this blog',
+      desc: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+      authorID: 13
+    },
+    {
+      id: '4',
+      thumbnail: Thumbnail4,
+      category: 'farming',
+      title: 'This is the title of the very first post on this blog',
+      desc: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+      authorID: 11
+    }
+  ]
+  
+  
